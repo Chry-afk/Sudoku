72448457836db2ddd1373c3ecac052833b6ac79b
Pietro Mannino <pietro.mannino@itsincom.it>
2024-02-01 14:14:06 +0100

    Metodo controllo matrice
    -metodo checkmatrix() che controlla la matrice per i numeri da 1 a 9

--- Sudoku/Sudoku/Program.cs
+++ Sudoku/Sudoku/Program.cs
@@ -6,32 +6,68 @@ using System.Threading.Tasks;
 
 namespace Sudoku
 {
-    internal class Program
+
+
+    class Program
     {
         static void Main(string[] args)
         {
-            int [,] grid = new int[9, 9];
-         
-            Random rand = new Random();
-            int numbersToRemove = 40; 
-
-            for (int i = 0; i < numbersToRemove; i++)
-            {
-                int row = rand.Next(9);
-                int col = rand.Next(9);
-                grid[row, col] = rand.Next(9);
-            }
-
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    Console.Write(grid[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
+            Sudoku sudoku = new Sudoku();
+            Console.WriteLine(sudoku.checkMatrix());
             Console.ReadKey();
         }
         
+    }
+
+    class Sudoku
+    {
+        int[,] matrix =
+            {
+                { '0', '0', '0', '0', '0', '0', '0', '0', '0' },
+                { '0', '0', '0', '0', '0', '0', '0', '0', '0' },
+                { '0', '0', '0', '0', '0', '0', '0', '0', '0' },
+                { '0', '0', '0', '0', '0', '0', '0', '0', '0' },
+                { '0', '0', '0', '0', '0', '0', '0', '0', '0' },
+                { '0', '0', '0', '0', '0', '0', '0', '0', '0' },
+                { '0', '0', '0', '0', '0', '0', '0', '0', '0' },
+                { '0', '0', '0', '0', '0', '0', '0', '0', '0' },
+                { '0', '0', '0', '0', '0', '0', '0', '0', '0' }
+            };
+
+        // metodo che controlla se la matrice è corretta o errata
+        public bool checkMatrix()
+        {
+            for (int number = 1; number <= 9; number++) {
+                for (int i = 0; i < 9; i++)
+                {
+                    int[] rigaArray = new int[9];
+                    for (int j = 0; j < 9; j++)
+                    {
+                        rigaArray[j] = matrix[i, j];
+                    }
+                    if (rigaArray.Contains(number)) return true;
+                }
+
+                for (int i = 0; i < 9; i++)
+                {
+                    int[] colonnaArray = new int[9];
+                    for (int j = 0; j < 9; j++)
+                    {
+                        colonnaArray[j] = matrix[j, i];
+                    }
+                    if (colonnaArray.Contains(number)) return true;
+                }
+            }
+            return false;
+        }
+
+        public bool insert(int number, int x, int y)
+        {
+            if (x<10 && x>0 && y < 10 && y > 0)
+            {
+                matrix[x, y] = number;
+            }
+            return false;
+        }
     }
 }
